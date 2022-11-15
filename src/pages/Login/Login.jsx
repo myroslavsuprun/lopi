5be48ebddf985867e32b6b72a2ef07d8c9aa8a13
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-15 13:46:12 +0200

    pretiered forms and components

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,49 +1,39 @@
-import React, {useState, useRef} from 'react';
-import { FormInput, Button } from 'components'
-
+import React, { useState } from "react";
+import { FormInput, Button } from "components";
+import { Container } from "@mui/system";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
 
-    
-
-    const submitHandler = event => {
-        event.preventDefault();
-        console.log(email, password);
-    }
-
-    
   return (
-    <form className='form' onSubmit={submitHandler} style={{ marginTop: '100px' }}>
-       
-        <FormInput 
-            label='Email:'
-            id="email"
-            change={setEmail} 
-            cont={email}
-            type='email'
+    <Container maxWidth="xs">
+      <form onSubmit={submitHandler}>
+        <FormInput
+          label="Email:"
+          id="email"
+          change={setEmail}
+          cont={email}
+          type="email"
         />
-        <br />
-        <FormInput 
-            label='Pasword:'
-            id="password"
-            change={setPassword} 
-            cont={password}
-            type='password'
-        /><br />
-        {/* <div className="form-gruop">
-            <label htmlFor="email">email login</label>
-            <input type="text" id='email' onChange={ e => setEmail(e.target.value)} />
-            
-        </div> */}
 
-        <Button type='submit' text='Submit' variant='primary' />
+        <FormInput
+          label="Pasword:"
+          id="password"
+          change={setPassword}
+          cont={password}
+          type="password"
+        />
 
-        
-    </form>
-  )
+        <Button type="submit" text="Submit" variant="primary" />
+      </form>
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
