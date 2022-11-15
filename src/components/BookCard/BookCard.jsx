611210b0296d27359811939ccbe533b13ceb9a5a
MiroProger <sirmiroslavsuprun@gmail.com>
2022-11-15 13:49:55 +0200

    Changed import type

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,8 +1,10 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+} from "@mui/material";
 
 const BookCard = () => {
   return (
