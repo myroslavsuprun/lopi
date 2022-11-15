be675c80f6685cd0cbc0976be9a98f95b0a7134f
MiroProger <sirmiroslavsuprun@gmail.com>
2022-11-15 11:30:21 +0200

    Added basic route structure for the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={Home}>
+        {/* Route log-in log in form  */}
+        {/* Route log out log out form */}
+        {/* Route books - booksList */}
+        {/* registration - registartion of books */}
+        {/* Route lending - lending of books */}
+        {/* Route return - returning of books */}
+        {/* Route statistics - statistics of books */}
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
