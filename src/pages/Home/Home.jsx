44a744326ea835c0e6494c910b677c7336ad398a
MiroProger <sirmiroslavsuprun@gmail.com>
2022-11-15 11:54:29 +0200

    Added header component for Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 
+import { Header } from "components";
+
 const Home = () => {
   return (
     <>
-      <div>Home Page</div>
+      <Header />
       <Outlet />
     </>
   );
